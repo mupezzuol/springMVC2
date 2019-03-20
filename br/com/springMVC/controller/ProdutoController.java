56c5d8c46cc5c8958d24def4592eab303e1d0424
mupezzuol <murillopezzuol@hotmail.com>
2019-03-19 21:14:01 -0300

    @CacheEvict -> Limpando as entradas e atualizando o cache a cada novo cadastro.

--- br/com/springMVC/controller/ProdutoController.java
+++ br/com/springMVC/controller/ProdutoController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.WebDataBinder;
@@ -38,6 +39,7 @@ public class ProdutoController  {
 	}
 	
 	@RequestMapping(value="/gravar", method=RequestMethod.POST)
+	@CacheEvict(value="produtosHome",allEntries=true)//Limpa todas as entradas e busca de novo a lista atualizada e o CACHE fica atualizado
 	public ModelAndView gravar(MultipartFile sumario, @Valid Produto produto, BindingResult result, RedirectAttributes redirectAttributes) {
 		
 		if(result.hasErrors()){
