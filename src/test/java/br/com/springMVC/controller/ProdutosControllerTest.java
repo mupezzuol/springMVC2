030111e952470690e12fcc2f837ec78f735471f2
mupezzuol <murillopezzuol@hotmail.com>
2019-04-10 21:16:19 -0300

    Spring Test; Testando as Controllers; MockMvc;

---
+++ src/test/java/br/com/springMVC/controller/ProdutosControllerTest.java
@@ -0,0 +1,53 @@
+package br.com.springMVC.controller;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import br.com.springMVC.conf.AppWebConfiguration;
+import br.com.springMVC.conf.DataSourceConfigurationTest;
+import br.com.springMVC.conf.JPAConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration //Puxa as configurações de contexto, onde tem configs de página, etc....
+@ContextConfiguration(classes = {JPAConfiguration.class, AppWebConfiguration.class, 
+		DataSourceConfigurationTest.class})
+@ActiveProfiles("test")
+public class ProdutosControllerTest {
+	
+	@Autowired
+    private WebApplicationContext wac;//Spring injeta nossas configs de webContext da aplicação
+	
+	private MockMvc mockMvc;
+	
+	
+	//Ele roda antes do teste, com a tag 'Before'
+	//ANTES de executar os testes nós inicializamos o webAppContext, que é nossa config já configurada na aplicação
+	//O spring injeta para nós com o '@Autowired'
+	@Before
+	public void setup(){
+	    mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).build();//Mandamos fazer o build do nosso webContext injetado pelo Spring
+	}
+	
+	
+	//Fazemos um 'get' na URL X, e esperamos um retorno para a página X
+	//Ele retorna caso nossa requisição esteja dessa forma
+	@Test
+	public void deveRetornarParaHomeComOsLivros() throws Exception{
+	    mockMvc.perform(MockMvcRequestBuilders.get("/"))//Requisição via GET para URL '/'
+	            .andExpect(MockMvcResultMatchers.model().attributeExists("produtos"))//Atributo que nós enviamos para a página abaixo
+	            .andExpect(MockMvcResultMatchers.forwardedUrl("/WEB-INF/views/home.jsp"));//Deve ser direcionado para essa página
+	}
+	
+	
+}
